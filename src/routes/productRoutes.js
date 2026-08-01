de4b41d833b99827b784258093197742a2a62ddd
ShahnawazSaddam16 <saddammaqsood4321@gmail.com>
2026-08-02 01:50:50 +0500

    adding authMiddleware in productRoutes

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -5,7 +5,7 @@ const authMiddleware = require("../middleware/authMiddleware");
 const upload = require("../middleware/upload");
 const {createProducts, fetchProducts, fetchProductsByCategory, deleteProducts} = require("../controllers/products");
 
-router.post("/create-product", limiter, upload.single("productImage"), createProducts);
+router.post("/create-product",  authMiddleware, limiter, upload.single("productImage"), createProducts);
 router.get("/all-products", fetchProducts);
 router.get("/category/:slug", fetchProductsByCategory);
 router.delete("/delete-product/:id", limiter, deleteProducts);
