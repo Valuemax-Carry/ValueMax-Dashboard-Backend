7b4dad5b03bef2deb29104eb21a90412171eb8b9
ShahnawazSaddam16 <saddammaqsood4321@gmail.com>
2026-08-06 16:36:14 +0500

    reducing age limit of auth

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -37,7 +37,7 @@ const Login = async (req, res) => {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: "strict",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
+      maxAge: 24 * 60 * 60 * 1000,
     });
 
     return res.status(200).json({
